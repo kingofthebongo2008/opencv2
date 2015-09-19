9acd10a319a7ed3bc6c991fb747a14110e52a52e
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-09-19 09:16:44 +0300

    reduced code size

--- bin/windows/release/run.bat
+++ bin/windows/release/run.bat
@@ -1,2 +1,2 @@
-stitching.exe p986.jpg p987.jpg p988.jpg p989.jpg p990.jpg p991.jpg p992.jpg p993.jpg
+stitching.exe --try_use_gpu no p986.jpg p987.jpg p988.jpg p989.jpg p990.jpg p991.jpg p992.jpg p993.jpg
 
