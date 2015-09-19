9454390f5ed8080a43baed948bdde072621177d0
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-09-19 09:16:52 +0300

    more

--- include/cvconfig.h
+++ include/cvconfig.h
@@ -2,3 +2,25 @@
 
 #define HAVE_JPEG
 
+//#define HAVE_CUDA
+//#define USE_CUDA
+
+//#define HAVE_OPENCV_GPU
+
+#define HAVE_WINRT
+
+#define CUDA_ARCH_BIN " 30"
+
+/* Create PTX or BIN for 1.0 compute capability */
+/* #undef CUDA_ARCH_BIN_OR_PTX_10 */
+
+/* NVIDIA GPU features are used */
+#define CUDA_ARCH_FEATURES " 30"
+
+/* Compile for 'virtual' NVIDIA PTX architectures */
+#define CUDA_ARCH_PTX "30"
+
+#define OPENCV_STICHING_CODE
+
+
+
